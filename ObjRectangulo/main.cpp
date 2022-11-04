7bc828bb2731b8c1b2db0c28a6405122c0a3177f
Jorge Montes <72566782+morjo2412@users.noreply.github.com>
2022-11-04 09:53:04 -0600

    Create main.cpp

---
+++ ObjRectangulo/main.cpp
@@ -0,0 +1,10 @@
+//Construya una clase llamada rectangulo que tengo los siguientes atributos: largo y ancho, y los
+//siguientes metodos: perimetro y area
+#include "rectangulo.h"
+
+
+int main() {
+  Rectangulo rectangulo1 (11,7);
+  rectangulo1.area();
+  rectangulo1.perimetro() ;
+}
