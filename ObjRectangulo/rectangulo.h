3eb557771fd075992b366e81b44db268e2a93e6b
Jorge Montes <72566782+morjo2412@users.noreply.github.com>
2022-11-04 09:55:22 -0600

    Create rectangulo.h

---
+++ ObjRectangulo/rectangulo.h
@@ -0,0 +1,16 @@
+#ifndef UNTITLED_RECTANGULO_H
+#define UNTITLED_RECTANGULO_H
+
+#include <iostream>
+#include <stdlib.h>
+
+class Rectangulo{
+private: //Atributos
+    float   largo,ancho;
+public: //metodos
+    Rectangulo (float,float); //constructor
+    void area();
+    void perimetro();
+};
+
+#endif //UNTITLED_RECTANGULO_H
